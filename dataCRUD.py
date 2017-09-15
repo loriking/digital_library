ed61e0ed9d8ecaac8b498e929a6b5016e3ca2b57
Lori King <loriking@users.noreply.github.com>
2017-09-15 19:41:04 +0200

    Create CRUD functions for languages, keywords, authors tables

---
+++ dataCRUD.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+'''
+dataCRUD.py
+Created on Sep 15, 2017
+
+@author: lak
+'''
+import sqlite3 as sql
+
+db = sql.connect('library_data.db')
+c = db.cursor()
+
+
+# Languages C.R.U.D.
+def add_language():
+    ''' Adds language to SQL database'''
+    language = input("Language:\t")
+    language = language.title()
+    c.execute('''INSERT INTO languages(language) VALUES(?)''', (language,))
+    db.commit()
+    
+def list_languages():
+    ''' Returns all the languages from database'''
+    c.execute('''SELECT * FROM languages''')
+    results = c.fetchall()
+    for i in results:
+        print(i)
+    return results
+    
+def get_languageID():
+    ''' Returns the ID (PK) of a given language'''
+    language = input("Enter language:\t")
+    language = language.title()
+    c.execute('''SELECT ID FROM languages WHERE language = ? ''', (language,))
+    langID = c.fetchone()[0]
+    print(language, "has ID of ", langID)
+    return langID
+    
+def get_language():
+    ''' Returns language from ID'''
+    langID = get_languageID()
+    c.execute('''SELECT language FROM languages WHERE ID = ?''', (langID,))
+    return c.fetchall()[0]
+    
+def update_language():
+    langID = get_languageID()
+    language = input("Correction:\t") 
+    print("Updating", language)
+    c.execute('''UPDATE languages SET language = ? WHERE ID =?''', (language, langID))
+    db.commit()
+    
+def delete_language():
+    languageID = get_languageID()
+    c.execute('''DELETE FROM languages WHERE ID = ?''', (languageID,))
+    print("Deleting item: ", languageID)
+    db.commit()
+    
+
+
+######## KEYWORDS CRUD FUNCTIONS
+
+
+def add_keyword():
+    ''' Adds language to SQL database'''
+    keyword = input("keyword:\t")
+    keyword = keyword.title()
+    c.execute('''INSERT INTO keywords(keyword) VALUES(?)''', (keyword,))
+    db.commit()
+    
+def list_keyword():
+    ''' Returns all the languages from database'''
+    c.execute('''SELECT * FROM keywords''')
+    results = c.fetchall()
+    for i in results:
+        print(i)
+    return results
+    
+def get_keywordID():
+    ''' Returns the ID (PK) of a given language'''
+    keyword = input("Enter keyword:\t")
+    keyword = keyword.title()
+    c.execute('''SELECT ID FROM keywords WHERE keyword = ? ''', (keyword,))
+    keywordID = c.fetchone()[0]
+    print(keyword, "has ID of ", keywordID)
+    return keywordID
+    
+def get_keyword():
+    ''' Returns kewword from ID'''
+    keywordID = input("Enter ID for keyword desired:\t")
+    c.execute('''SELECT keyword FROM keywords WHERE ID = ?''', (keywordID,))
+    return c.fetchall()[0]
+    
+def update_keyword():
+    keywordID = get_keywordID()
+    keyword = input("Correction:\t") 
+    print("Updating", keyword)
+    c.execute('''UPDATE keywords SET keyword = ? WHERE ID =?''', (keyword, keywordID))
+    db.commit()
+    
+def delete_keyword():
+    keywordID = get_keywordID()
+    c.execute('''DELETE FROM keywords WHERE ID = ?''', (keywordID,))
+    print("Deleting item: ", keywordID)
+    db.commit()
+
+############## AUTHORS ##################
+
+def add_author():
+    ''' Adds language to SQL database'''
+    first_name = input("Author first name:\t")
+    last_name = input("Author last name:\t")
+    
+    first_name = first_name.title()
+    last_name = last_name.title()
+    c.execute('''INSERT INTO authors(first_name, last_name) VALUES(?, ?)''', (first_name, last_name))
+    db.commit()
+    
+def list_authors():
+    ''' Returns all the languages from database'''
+    c.execute('''SELECT * FROM authors''')
+    results = c.fetchall()
+    for i in results:
+        print(i)
+    return results
+    
+def get_authorID():
+    ''' Returns the ID (PK) of a given language'''
+    
+    search_term = input("Search by first name, last name or full name? Enter: f/l/b)\n")
+    if search_term.lower() == "f":
+        first_name = input("Enter first name:\t")
+        first_name = first_name.title()
+        c.execute('''SELECT ID FROM authors where first_name = ?''', (first_name,))
+        results =  c.fetchall()[0]
+    elif search_term.lower() == "l":
+        last_name = input("Enter last name:\t")
+        last_name = last_name.title()
+        results = c.execute('''SELECT ID FROM authors where last_name = ?''', (last_name,))
+        results =  c.fetchall()[0]
+    elif search_term.lower() == "b":
+        first_name = input("Enter first name:\t")
+        last_name = input("Enter last name:\t")
+        results = c.execute('''SELECT ID FROM authors where first_name = ? AND last_name = ?''', 
+                  (first_name, last_name,))
+        results = c.fetchall()[0]
+    
+    return results
+
+
+    
+def get_author():
+    ''' Returns author from ID'''
+    authorID = input("Enter ID for author desired:\t")
+    c.execute('''SELECT first_name, last_name FROM authors WHERE ID = ?''', (authorID,))
+    return c.fetchall()[0]
+
+
+    
+def update_author():
+    authorID = get_authorID()
+    author = input("Correction:\t") 
+    print("Updating", author)
+    c.execute('''UPDATE authors SET author = ? WHERE ID =?''', (author, authorID))
+    db.commit()
+    
+def delete_author():
+    authorID = get_authorID()
+    c.execute('''DELETE FROM authors WHERE ID = ?''', (authorID,))
+    print("Deleting item: ", authorID)
+    db.commit()
+
+
+
