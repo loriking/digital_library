6736fc7fad3a98cb84e0eca1f9ba5ea2167964e0
Lori King <codedinspain@gmail.com>
2017-10-03 16:16:15 +0200

    Add homepage and switch windows functionality

--- GUI_tk.py
+++ GUI_tk.py
@@ -7,20 +7,62 @@ import dataCRUD as data
 class ProjectLibrary(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.main = tk.Frame()
-        self.data = data
 
-        topframe = ttk.LabelFrame(self, text="", padding='0.2i', borderwidth=0)
-        topframe.pack()
+        main = tk.Frame(self)
+        main.pack(side="top", fill="both", expand=True)
+        main.grid_rowconfigure(0, weight=1)
+        main.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (Homepage, Addproject, Addresource):
+            frame = F(main, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(Homepage)
+
+    def show_frame(self, cont):
+        ''' raises this frame in the stacking order'''
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class Homepage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
         self.authors = data.list_authors()
 
-        authors = ttk.Button(topframe, text = 'authors', command  = lambda: self.get_authors())
-        authors.pack()
+        topframe = ttk.LabelFrame(self, text="", padding='0.2i', borderwidth=0)
+        topframe.grid(column = 0, row = 0)
 
-    def get_authors(self):
-        for item in self.authors:
-            print(item)
+        newproject = ttk.Button(topframe, text = "Add Project",
+                                command=lambda: controller.show_frame(Addproject))
+        newproject.grid(column = 1, row = 0)
+
+        newresource = ttk.Button(topframe, text = "Add Resource",
+                                 command = lambda: controller.show_frame(Addresource))
+        newresource.grid(column = 2, row = 0)
+
+class Addresource(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text = "Resources")
+        label.grid(row = 0, column = 0, columnspan = 4, pady = 10, padx  =10)
+
+        topframe = ttk.LabelFrame(self, text = "", padding='0.2i', borderwidth=0)
+        topframe.grid(column = 0, row = 2)
+
+
+
+class Addproject(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.label = tk.Label(self, text="Menus")
+        self.label.grid(row=0, column=0, columnspan=4)
 
 
 
